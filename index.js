406debaf64926563168abd661cbd8c698ad84e44
brenoluna <brenoefl@gmail.com>
2019-08-26 16:32:23 -0300

    Completed async/await exercise

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 console.log('before');
+// Promise-based approach
+// getUser(1)
+//     .then(user => getRepos(user.gitHubUsername))
+//     .then(repos => getCommits(repos[0]))
+//     .then(commits => console.log('Commits', commits))
+//     .catch(err => console.log('Error', err.message));
 
-getUser(1)
-    .then(user => getRepos(user.gitHubUsername))
-    .then(repos => getCommits(repos[0]))
-    .then(commits => console.log('Commits', commits))
-    .catch(err => console.log('Error', err.message));
+// Async and Await approach
+// The awaited functions are wrapped by an async function
+// 'try/catch' blocks are used for error handling
+async function displayCommits() {
+    try {
+        const user = await getUser(1);
+        const repos = await getRepos(user.gitHubUsername);
+        const commits = await getCommits(repos[0]);
+        console.log(commits);
+    }
+    catch (err) {
+        console.log('Error', err.message);
+    }
+}
+displayCommits();
 
 console.log('after');
 
@@ -22,6 +38,7 @@ function getRepos(username) {
         setTimeout(() => {
             console.log(`Fetching ${username}'s repositories...`);
             resolve(['repo1','repo2','repo3']);
+            //reject(new Error('something went wrong'));
         }, 2000);
     });
 }
